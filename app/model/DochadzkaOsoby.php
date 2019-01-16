73309806a38493e9be1627b7aabd6dd8bb9fd716
vbelco-pc <vbelco-pc@192.168.100.5>
2019-01-16 20:59:25 +0100

    user management

--- app/model/DochadzkaOsoby.php
+++ app/model/DochadzkaOsoby.php
@@ -27,6 +27,9 @@ class DochadzkaOsoby extends Nette\Object {
     public $dochadzka_prichod; //pole dochadzky v casovom rozmedzi, indexy v poli su id riadkov z databazy dochadzka
     public $dochadzka_odchod; //pole dochadzky odchodov v casovom rozmedzi, indexy v poli su id riadkov z databazy dochadzka
     
+    public $dochadzka_prichod_upravene; //pole prichodov, ktore boli uz upravovane
+    public $dochadzka_odchod_upravene; //dtto o odchodoch
+    
     function __construct(Nette\Database\Context $database, $user_id, $osoba_id)  {
         $this->database = $database;
         $this->user_id = $user_id;
@@ -44,21 +47,31 @@ class DochadzkaOsoby extends Nette\Object {
     }
     
     public function nacitaj ( $od, $do ){
-        $this->od = $this->createArrivalDatetime( $od );;
+        $this->od = $this->createArrivalDatetime( $od );
         $this->do = $this->createLeaveDatetime( $do );
         //natiahneme riadky s dochadzko v danom termine
         $dni = $this->database->table('dochadzka')
                     ->where ('users_id = ?', $this->user_id )
                     ->where ('people_id = ?', $this->osoba_id )
                     ->where ('den >= ?', $this->od->format('Y-m-d H:i:s') )
-                    ->where ('den <= ?', $this->do->format('Y-m-d H:i:s') )
-                    ->fetchall();
-        
-        foreach ($dni as $index => $den) {
+                    ->where ('den <= ?', $this->do->format('Y-m-d H:i:s') );
+        //nahadzeme si udaje o dochadzke do objektu
+        if ( $dni->count() != 0 ) {
+            foreach ($dni as $index => $den) {
                 $this->dochadzka_prichod[$index] = $den["prichod_timestamp"];
                 $this->dochadzka_odchod[$index] = $den["odchod_timestamp"];
                 $this->dochadzka_den[$index] = $den["den"];
-        }
+                $this->dochadzka_prichod_upravene[$index] = $den["upr_prichod_timestamp"];
+                $this->dochadzka_odchod_upravene[$index] = $den["upr_odchod_timestamp"];
+            }//end foreach
+        }//end if pocet je viac ako 0
+        else {
+            $this->dochadzka_den = NULL;
+            $this->dochadzka_prichod = NULL;
+            $this->dochadzka_odchod = NULL;
+            $this->dochadzka_odchod_upravene = NULL;
+            $this->dochadzka_prichod_upravene = NULL;
+        }// end else, nemame definovanu dochadzku pre tohto cloveka v danom termine
     }//end function nacitaj dochadzku
     
     //funckia zmeni hodnotu dochazdky
@@ -69,19 +82,35 @@ class DochadzkaOsoby extends Nette\Object {
         //$hodnota je v tvare hh:mm, musime k nej pridat aj datum, ten si ziskame pomocou id riadku
         $vysl = $this->database->table('dochadzka')->get($zaznam_id);
         $nova_hodnota = new Nette\Utils\DateTime( $vysl["den"] );
-        $nova_hodnota->modify($hodnota);
+        $nova_hodnota->modify($hodnota); //ku dnu pripocitame hodiny
         switch ($typ){
             case "prichod":
+                $stara_hodnota = $vysl["prichod_timestamp"]; //nacitame si staru hodnotu, moye bzt ja NULL
                 $pole = array ( 'prichod_timestamp' => $nova_hodnota, 
-                                'upr_prichod_timestamp' => $nova_hodnota);
+                                'upr_prichod_timestamp' => $nova_hodnota); //pripravime si meniaci sql dotaz
+                $pole_insert = array( 'typ' => 'prichod',
+                                      'stary' => $stara_hodnota,
+                                      'novy' => $nova_hodnota,
+                                      'users_id' => $this->user_id );
                 break;
             case "odchod":
+                $stara_hodnota = $vysl["odchod_timestamp"];
                 $pole = array ( 'odchod_timestamp' => $nova_hodnota, 
                                 'upr_odchod_timestamp' => $nova_hodnota);
+                $pole_insert = array( 'typ' => 'odchod',
+                                      'stary' => $stara_hodnota,
+                                      'novy' => $nova_hodnota,
+                                      'users_id' => $this->user_id );
                 break;
         }//end switch
-        $row= $this->database->table('dochadzka')
+        try {
+            $row= $this->database->table('dochadzka')
                             ->where('id = ?', $zaznam_id)
                             ->update($pole);
+            $row2= $this->database->table('dochadzka_log_zmeny')
+                            ->insert($pole_insert);
+        } catch ( Nette\Database\ConnectionException $e  ){
+            throw new \ErrorException;
+        }
     }//end funkcia storeDochadzka
 }
